affff341f6f2a4e16535fc48a4f6d988ce6c70d3
Nate Schroeder <n8dogg59@hotmail.com>
2021-05-02 18:49:42 -0500

    fixed offline problem

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const APP_PREFIX = "Budget-";
 const VERSION = "version_02";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
+  "/",
   "./index.html",
   "./css/styles.css",
   "./js/idb.js",
@@ -63,37 +64,73 @@ self.addEventListener("activate", function (e) {
 // });
 
 // // Respond with cached resources
-self.addEventListener("fetch", function (e) {
-  if (e.request.url.includes("/api/")) {
-    console.log("fetch request : " + e.request.url);
+// self.addEventListener("fetch", function (e) {
+//   if (e.request.url.includes("/api/")) {
+//     console.log("fetch request : " + e.request.url);
+//     e.respondWith(
+//       caches.open(CACHE_NAME).then(function (e) {
+//         return fetch(e.request)
+//           .then((response) => {
+//             if (response.status === 200) {
+//               cache.put(e.request.url, response.clone());
+//             }
+//             return response;
+//           })
+//           .catch((err) => {
+//             return cache.match(e.request);
+//           });
+//       })
+//     );
+//     return;
+//   } 
+//   e.respondWith(
+//     fetch(e.request).catch(function() {
+//       return caches.match(e.request).then(function(response) {
+//         if (response) {
+//           return response;
+//         } else if (e.request.headers.get("accept").includes("text/html")) {
+//           // return the cached home page for all requests for html pages
+//           return caches.match("/");
+//         }
+//       });
+//     })
+//   );
+// });
+
+self.addEventListener('fetch', function (e) {
+  console.log('from fetch');
+  if (e.request.url.includes('/api/')) {
+    console.log('fetch request : ' + e.request.url);
     e.respondWith(
-      caches.open(CACHE_NAME).then(function (e) {
-        return fetch(e.request)
-          .then((response) => {
-            if (response.status === 200) {
-              cache.put(e.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch((err) => {
-            return cache.match(e.request);
-          });
-      })
+      // caches.open(CACHE_NAME).then(function (e) {
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(e.request);
+            });
+        })
+        .catch((err) => console.log('yo' + err)),
     );
     return;
-  } 
+  }
   e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request).then(function(response) {
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
         if (response) {
           return response;
-        } else if (e.request.headers.get("accept").includes("text/html")) {
+        } else if (e.request.headers.get('accept').includes('text/html')) {
           // return the cached home page for all requests for html pages
-          return caches.match("/");
+          return caches.match('/');
         }
       });
-    })
+    }),
   );
-
-  
 });
